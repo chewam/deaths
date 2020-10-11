312fc72720c9c942ac6d0978062fe72501ccc84d
Chewam <gary@chewam.com>
2020-10-11 19:56:33 +0200

    Run fetch script.

--- scripts/build.js
+++ scripts/build.js
@@ -87,7 +87,7 @@ const getFiles = () => {
         console.error("Could not list the directory.", err)
         process.exit(1)
       }
-      resolve(files)
+      resolve(files.filter((file) => file.match(/^.+\.txt$/)))
     })
   })
 }
@@ -105,4 +105,4 @@ const main = async () => {
   console.log("Result written into", resultFilePath)
 }
 
-main()
+module.exports = main
