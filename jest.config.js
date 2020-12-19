d2cbe09f5c8855db0aa791c8487b90c928c27d0f
Chewam <gary@chewam.com>
2020-12-19 17:30:30 +0100

    Set jest coverageThreshold.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   collectCoverageFrom: [
     "src/**/**.js",
     "!**/node_modules/**",
