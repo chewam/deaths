3624e4587274a131508e5cc481ae8456abec7d17
chewam <gary@chewam.com>
2021-05-06 03:04:20 +0200

    fix: Handle error pages.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
+import App from "next/app"
+import { useEffect } from "react"
+import ErrorPage from "next/error"
 import Head from "@/components/Head"
-import type { AppProps } from "next/app"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import { Themes } from "@/services/themes"
 import FiltersBar from "@/components/FiltersBar"
+import type { AppProps, AppContext } from "next/app"
 
 import "@/styles/index.scss"
 import dark from "@/styles/themes/dark.module.scss"
 import light from "@/styles/themes/light.module.scss"
-
-import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   // Remove the server-side injected CSS to avoid Material-UI slider flickering
@@ -20,6 +21,10 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       parentElement?.removeChild(jssStyles)
     }
   }, [])
+
+  if (pageProps?.statusCode >= 400) {
+    return <ErrorPage statusCode={pageProps.statusCode} />
+  }
 
   return (
     <Themes light={light} dark={dark}>
