a4bbc1549f0a9606b03f83d1ebdd2990b5eef449
chewam <gary@chewam.com>
2021-10-03 04:12:27 +0200

    chore(security): Add CSP headers

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
 const { version } = require("./package.json")
 const { withSentryConfig } = require('@sentry/nextjs');
 
+const securityHeaders = [{
+  key: "Content-Security-Policy",
+  value: "default-src 'self'"
+}]
+
 const moduleExports = {
   env: { APP_VERSION: version },
   i18n: {
     defaultLocale: "en",
     locales: ["fr", "en"],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
   }
 };
 
