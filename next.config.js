c2bee073fda1b944d4bf58866867bc00a1e19897
chewam <gary@chewam.com>
2022-08-06 05:20:17 +0200

    add csp

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ if (analyzerEnabled) {
     script-src 'self' *.sentry.io *.googletagmanager.com 'sha256-Knm+Tl38SOjwUxvDOr3uFM81svhZ9Twnt6mYVgF2K9s=';
     style-src 'self' ${stylesHashes} 'unsafe-hashes';
     font-src 'self';
-    connect-src 'self' vitals.vercel-insights.com;
+    connect-src 'self' *.sentry.io vitals.vercel-insights.com;
   `
 
   const securityHeaders = [{
