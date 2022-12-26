040a67342601ecc5840f8044ec6922d5e26e485b
Chewam <gary@chewam.com>
2022-12-26 06:58:57 +0100

    fix: add title to language links (#118)
    * fix: add title to language links
    
    * fix prettier

--- src/components/Lang.tsx
+++ src/components/Lang.tsx
@@ -5,9 +5,10 @@ const Lang = () => {
   const router = useRouter()
   const { locale, pathname } = router
   const lang = locale === "fr" ? "en" : "fr"
+  const title = locale === "fr" ? "english" : "français"
 
   return (
-    <Link href={pathname} locale={lang} className="lang">
+    <Link href={pathname} locale={lang} className="lang" title={title}>
       {lang}
     </Link>
   )
