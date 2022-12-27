08b3ce8fe55aebdbce4ea767511c37113f3f0a24
Chewam <gary@chewam.com>
2022-12-27 07:12:47 +0100

    chore: generate sitemap on build (#144)
    * chore: generate sitemap on build
    
    * index as /

---
+++ scripts/sitemap.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+"use strict"
+
+const fs = require("fs")
+const path = require("path")
+
+const BASE_URL = "https://deaths.chewam.com"
+
+const urls = fs
+  .readdirSync("src/pages")
+  .filter((pages) => {
+    return ![
+      "_app.tsx",
+      "_document.tsx"
+    ].includes(pages)
+  })
+  .map((page) => {
+    const name = path.parse(page).name
+    return `${BASE_URL}/${name === "index" ? "" : name}`
+  })
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls
+      .map((url) => {
+        return `
+          <url>
+            <loc>${url}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>1.0</priority>
+          </url>
+        `;
+      })
+      .join("")}
+  </urlset>
+`;
+
+fs.writeFileSync('public/sitemap.xml', sitemap)
