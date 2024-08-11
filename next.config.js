d83ea63b86888dc0cfb46a7b4542d3095c5888d4
chewam <gary@chewam.com>
2024-08-11 17:03:40 +0200

    fix(csp): remove nonce

--- next.config.js
+++ next.config.js
@@ -13,6 +13,35 @@ const SentryWebpackPluginOptions = {};
 
 const config = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+ 
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
+}
+
 const analyzerEnabled = process.env.ANALYZE === "true"
 
 if (analyzerEnabled) {
